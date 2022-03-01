7c4b23c5ee65f02974a9187ca09c26d15ca1863b
Aman Kumar <kumaramansingh137@gmail.com>
2022-03-01 15:16:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+// JavaScript
+
+for (var i=0; i<5; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click" function() {
+    document.querySelector("h1").style.css = "purple";
+  });
+}
+
+
+// jQuery.
+
+$("button").click(function() {
+  $("h1").css("color", "purple");
+});
+
+
+$(document).keypress(function(event) {
+  $("h1").text(event.key);
+});
+
+
+$("h1").on("mouseover", function() {
+  $("h1").css("color", "purple");
+});
