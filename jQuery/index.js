48d178b0fc9b75ac96637fe4c75ed97c528127a6
Aman Kumar <kumaramansingh137@gmail.com>
2022-03-04 13:20:49 +0530

    Add files via upload

--- jQuery/index.js
+++ jQuery/index.js
@@ -23,3 +23,36 @@ $(document).keypress(function(event) {
 $("h1").on("mouseover", function() {
   $("h1").css("color", "purple");
 });
+
+$("h1").on("click", function() {
+   $("h1").hide();
+});
+
+
+$("button").on("click", function() {
+   $("h1").toggle();
+});
+
+$("button").on("click", function() {
+   $("h1").fadeOut();
+});
+
+$("button").on("click", function() {
+   $("h1").fadeToggle();
+});
+
+$("button").on("click", function() {
+   $("h1").slideUp();
+});
+
+$("button").on("click", function() {
+   $("h1").slideToggle();
+});
+
+$("button").on("click", function() {
+   $("h1").animate({opacity: 0.5});
+});
+
+$("button").on("click", function() {
+   $("h1").slideUp().slideDown().animate({opacity: 0.5});
+});
