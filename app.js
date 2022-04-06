e447494c4e4908058bcf01ee4db307be4ddbcdc4
Aman Kumar <kumaramansingh137@gmail.com>
2022-04-06 23:51:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+
+var number = 5;  // in-line comment
+
+/* This is a
+fdf
+fdsf
+multi-line comment */
+
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and Object
+*/
+
+// Declaring Variables
+
+var myName = "Aman";
+
+myName = 8;
+
+let ourName = "googlers";
+
+const pi = 3.14;
+
+// Storing Values with Assignment Operator
+
+var a;
+
+var b = 2;
+
+a = 7; // don't need to dclare because a is already declared
+
+b = a;
+
+console.log(a); // = 7
+
+// Initializing variables w/ Assignment Operator
+
+var a = 9;
+
+//Uninitialized variables
+// Initialize these three variables
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// DO not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// case sensitivity in variables
+// Declarations
+var studyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignmentss
+studyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Adding numbers
+var sum = 10 + 10;
+console.log(sum) // = 20
+
+// Subtraction numbers
+var difference = 45 - 33; // = 12
+
+
+// Escape Sequence in a string
+
+/***
+Code   Output
+\'     single quote
+\"     double quote
+\\     backslash
+\n     newline
+\r     carriage return
+\t     tab
+\b     backspace
+\f     form feed
+***/
+
+// Length of String
+
+var firstNameLength = 0;
+var firstName = "Aman";
+
+firstNameLength = firstName.length;
+
+//
